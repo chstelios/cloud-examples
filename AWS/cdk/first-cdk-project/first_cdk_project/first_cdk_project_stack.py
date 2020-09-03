1bb29aad2ffceea3352ae3ded335130dbf3c3ba6
Stelios Chliaoutakis <sch@Stelioss-MacBook-Pro.local>
2020-09-03 22:39:31 +0200

    Push first cdk app

---
+++ AWS/cdk/first-cdk-project/first_cdk_project/first_cdk_project_stack.py
@@ -0,0 +1,16 @@
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+)
+
+class FirstCdkProjectStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketID"
+        )
